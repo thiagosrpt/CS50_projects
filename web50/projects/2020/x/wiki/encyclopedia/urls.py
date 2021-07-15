3922b67b7296fcefbe3784d3b6956caf33868c8f
Thiago <Petrimperni@Nicholass-iMac.local>
2021-07-15 08:35:44 -0400

    good version

--- web50/projects/2020/x/wiki/encyclopedia/urls.py
+++ web50/projects/2020/x/wiki/encyclopedia/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.wiki, name="wiki")
+    path("wiki/", views.index, name="wiki_index"),
+    path("wiki/<str:title>", views.wiki, name="wiki"),
+    path("new/", views.new_entry, name="new_entry"),
+    path("random/", views.random_page, name="random_page"),
+    path("edit/<str:title>", views.edit, name="edit")
 ]
